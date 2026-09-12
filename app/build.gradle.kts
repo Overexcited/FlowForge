7676541a692fe1566311cb6683ff6b455736c43b
Overexcited <richardkurek99@gmail.com>
2026-09-12 11:58:56 +1000

    Last Sync (2026-09-12 11:58)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.flowforge.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.1.9"
+        versionCode = 21
+        versionName = "0.2.1"
     }
 
     signingConfigs {
